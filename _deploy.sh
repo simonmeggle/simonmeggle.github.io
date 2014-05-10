37cf94cd1da29f2b563e4e0fbbd8aba7c3a2a7d2
sylvaindurand <sylvaindurand@users.noreply.github.com>
2014-05-10 15:44:08 +0200

    Choose gzipping from file extention, not folder

--- _deploy.sh
+++ _deploy.sh
@@ -1,19 +1,27 @@
-
-# Compilation de Jekyll
+# Build Jekyll
 jekyll build
 
-# Optimisation et ompression de tous les fichiers
+# Compressing and optimizing files
 find _site -name '*.jpg' -exec jpegoptim --strip-all -m80 {} \;
 find _site -name '*.png' -exec optipng -o5 {} \;
-find _site -path _site/static -prune -o -type f -exec gzip -n "{}" \; -exec mv "{}.gz" "{}" \;
+find -E _site -type f -regex '.*\.(html|css|js|svg|xml)' -exec gzip -n "{}" \; -exec mv "{}.gz" "{}" \;
 
+# Sync gziped files
+s3cmd --acl-public \
+      --cf-invalidate -M \
+      --rinclude '.*\.(html|css|js|svg|xml)' \
+      --add-header="Cache-Control: max-age=604800" \
+      --add-header 'Content-Encoding:gzip' \
+      sync _site/ s3://sylvain.durand.tf/ 
 
+# Sync other files
+s3cmd --acl-public \
+      --cf-invalidate -M \
+      --rexclude '.*\.(html|css|js|svg|xml)' \
+      --add-header="Cache-Control: max-age=6048000" \
+      sync _site/ s3://sylvain.durand.tf/ 
 
-# Synchronisation des médias
-s3cmd  --acl-public --cf-invalidate --cf-invalidate-default-index -M --add-header="Cache-Control: max-age=6048000" sync _site/static s3://sylvain.durand.tf/ 
-
-# Synchronisation des autres fichiers
-s3cmd --acl-public --cf-invalidate --cf-invalidate-default-index -M  -m text/html --add-header 'Content-Encoding:gzip' --add-header="Cache-Control: max-age=604800" --exclude="/static/*" sync _site/ s3://sylvain.durand.tf/ 
-
-# Suppression des fichiers retirés en local
-s3cmd --delete-removed --cf-invalidate-default-index sync _site/ s3://sylvain.durand.tf/ 
+# Delete removed files
+s3cmd --delete-removed \
+      --cf-invalidate-default-index \
+      sync _site/ s3://sylvain.durand.tf/ 
