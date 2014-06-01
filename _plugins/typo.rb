072ca6f291a6009d7dc974893a78c26f6bfceda3
sylvaindurand <sylvaindurand@users.noreply.github.com>
2014-06-01 13:23:00 +0200

    Pretty date without plugin

--- _plugins/typo.rb
+++ _plugins/typo.rb
@@ -32,45 +32,8 @@ class Typography < String
 end
 
 module Typo
-
   def typo(text)
     Typography.new(text).to_html
-  end
-
-  def englishdate(date)
-    date = date.gsub('01 ', '1<sup>th</sup> ')
-    date = date.gsub('02 ', '2<sup>nd</sup> ')
-    date = date.gsub('03 ', '3<sup>rd</sup> ')
-    date = date.gsub('04 ', '4 ')
-    date = date.gsub('05 ', '5 ')
-    date = date.gsub('06 ', '6 ')
-    date = date.gsub('07 ', '7 ')
-    date = date.gsub('08 ', '8 ')
-    date = date.gsub('09 ', '9 ')
-  end
-
-  def frenchdate(date)
-    date = date.gsub('01 ', '1<sup>er</sup> ')
-    date = date.gsub('02 ', '2 ')
-    date = date.gsub('03 ', '3 ')
-    date = date.gsub('04 ', '4 ')
-    date = date.gsub('05 ', '5 ')
-    date = date.gsub('06 ', '6 ')
-    date = date.gsub('07 ', '7 ')
-    date = date.gsub('08 ', '8 ')
-    date = date.gsub('09 ', '9 ')    
-    date = date.gsub('January',  'janvier')
-    date = date.gsub('February', 'février')
-    date = date.gsub('March',    'mars')
-    date = date.gsub('April',    'avril')
-    date = date.gsub('May',      'mai')
-    date = date.gsub('June',     'juin')
-    date = date.gsub('July',     'juillet')
-    date = date.gsub('August',   'août')
-    date = date.gsub('September','septembre')
-    date = date.gsub('October',  'octobre')
-    date = date.gsub('November', 'novembre')
-    date = date.gsub('December', 'décembre')
   end
 end
 
