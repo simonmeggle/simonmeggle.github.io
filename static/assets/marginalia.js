9b09904b57dc9f71688906a8603524888b671b3b
sylvaindurand <sylvaindurand@users.noreply.github.com>
2014-06-23 19:29:23 +0200

    Bigger title and few design tweak

--- static/assets/marginalia.js
+++ static/assets/marginalia.js
@@ -2,14 +2,14 @@ var $ = function(el){return document.querySelectorAll(el);};
 
 function marginalia(){
     if(window.innerWidth > 1000){
-        $('.footnotes')[0].className="footnotes marginalia";
+        $('body')[0].className="marginalia";
         var nodes = $('.marginalia ol li'); var end = 0;
         for(var i=0;i<nodes.length;i++){
             position=$('[href="#'+nodes[i].getAttribute('id')+'"]')[0].getBoundingClientRect().top + window.pageYOffset;
             if(position<end){position=end}
             nodes[i].style.top = position+'px';
             var end = position+nodes[i].clientHeight+30;}}
-    else{ $('.marginalia')[0].className="footnotes"}}
+    else{ $('body')[0].className=""}}
 
 document.addEventListener('DOMContentLoaded', function(){marginalia();});
 window.onresize = function(event){marginalia();}
