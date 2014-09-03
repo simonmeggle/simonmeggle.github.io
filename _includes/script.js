18ae75f9dff0208fdcb85c320137a808156cba9e
sylvaindurand <sylvaindurand@users.noreply.github.com>
2014-09-03 23:12:06 +0200

    Readable code

--- _includes/script.js
+++ _includes/script.js
@@ -1,5 +1,3 @@
-{% capture js %}
-
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -43,5 +41,3 @@ window.onload = function(event){marginalia();};
   document.getElementsByTagName("head")[0].appendChild(script); })();
 
 {% endif %}
-
-{% endcapture %}{{ js | strip_newlines | remove: '  ' }}
